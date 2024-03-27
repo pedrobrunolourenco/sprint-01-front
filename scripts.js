5d3a15d74fd0d7e76fcb1b099ec012856e0af91f
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-03-27 19:08:19 -0300

    COMMIT-005

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ let nivel_ = 0;
 let id_base_ = 0;
 let nivel_atual = 0;
 let id_origem = 0;
+let origem = "";
+e_pai_ou_mae = "";
 
-const getList = async () => {
+const getList = () => {
     $('#div-membro-base').show();
     $('#div-membro-comum').hide();
-    let url = 'http://127.0.0.1:5000/membro_base';
+    let  url = 'http://127.0.0.1:5000/membro_base';
     fetch(url, {
       method: 'get',
     })
@@ -29,28 +31,64 @@ const getList = async () => {
   const getListMembroComum = async (item) => {
     id_base_ = item;
     $('#div-membro-base').hide();
-    $('#idform').hide();
     $('#div-membro-comum').show();
+    $('#idform').hide();
 
-    let url = 'http://127.0.0.1:5000/membro_comun?id_base=' + item;
+    let url =  'http://127.0.0.1:5000/membro_comun?id_base=' + item;
     fetch(url, {
       method: 'get'
     })
     .then((response) => response.json())
     .then((data) => {
       $('#table-geracao').find('tbody').detach();
-      $('#table-geracao').append($('<tbody>'));        
+      $('#table-geracao').append($('<tbody>'));  
       insertListMembroComum(data);
     })
       .catch((error) => {
         console.error('Error:', error);
       });
   }
+
+  const insertListMembroComum = async (data) => {
+    let geracao = 0;
+    let g1 = '<div class="card text-white bg-black mb-3">'
+    let g2 = '<div class="card-header text-center">'
+    let g3 = '</div>';
+    let g4 = '</div>';
+    let btpai01   = '<button type="button" class="btn btn-primary btn-sm"'
+    let btmae01   = '<button type="button" class="btn btn-info btn-sm"'
+    let btkid01 = '<button type="button" class="btn btn-warning btn-sm"'
+    let btfecha = '</button>';
+    if(data.membros != null)
+    {
+      for (let i = 0; i < data.membros.length; i++) {
+        let nivel = data.membros[i].nivel;
+        geracao ++;
+        var linha = "";
+        if(data.membros != null)
+        {
+           for (j = i; j < data.membros.length && data.membros[j].nivel == nivel; j++) {
+            linha =  '<tr>';
+            pnGeracao = g1 + g2 + geracao+'a. Geração' +g3 + g4;
+            linha += '<td>' + pnGeracao + '</td>';
+            let id = data.membros[j].id;
+            linha += '<td>' + btpai01 + 'onclick="add_pai(' + id + ',' + nivel + ')">' + data.membros[j].nome_pai + btfecha + '</td>';
+            linha += '<td>' + btmae01 + 'onclick="add_mae(' + id + ',' + nivel + ')">' + data.membros[j].nome_mae +  btfecha + '</td>';
+            linha += '<td>' + btkid01 + 'onclick="add_filho(' + id + ',' + nivel + ')">' + data.membros[j].nome + btfecha + '</td>';
+            linha += '</tr>'
+            $('#table-geracao').append(linha);              
+          };
+         i=j-1;
+        }
+      }
+    }
+  }
+
   
   
   const newItem = async () => {
     let inputNome = document.getElementById("newInput").value;
-    postItem(inputNome)
+    await postItem(inputNome)
     let nome_base = $('#newInput').val()
     $('#titulo-arvore').val(nome_base);
     $('#newInput').val('');
@@ -63,11 +101,11 @@ const getList = async () => {
     $('#div-membro-base').show();
   }
 
-  const postItem = async (inputNome) => {
+  const postItem = async (inputNome) =>  {
     const formData = new FormData();
     formData.append('nome', inputNome);
 
-    let url = 'http://127.0.0.1:5000/membro_base';
+    let url = await 'http://127.0.0.1:5000/membro_base';
     fetch(url, {
       method: 'post',
       body: formData
@@ -83,7 +121,7 @@ const getList = async () => {
     })
    }
 
-  const insertListMembroBase = (id, nome) => {
+  const insertListMembroBase = async (id, nome) => {
     let bt1 = '<button type="button" class="btn btn-success btn-sm"><span class="bi bi-tree"></span></button>';
     let bt2 = '<button type="button" class="btn btn-danger btn-sm"><span class="bi bi-trash-fill"></span></button>';
 
@@ -96,39 +134,11 @@ const getList = async () => {
     $('#table-base').append(employee_data); 
   }
 
-  const insertListMembroComum = (data) => {
-    let geracao = 0;
-    let g1 = '<div class="card text-white bg-black mb-3">'
-    let g2 = '<div class="card-header text-center">'
-    let g3 = '</div>';
-    let g4 = '</div>';
-    let btpai01   = '<button type="button" class="btn btn-primary btn-sm"'
-    let btmae01   = '<button type="button" class="btn btn-info btn-sm"'
-    let btkid01 = '<button type="button" class="btn btn-warning btn-sm"'
-    let btfecha = '</button>';
-    for (let i = 0; i <= data.membros.length; i++) {
-      let nivel = data.membros[i].nivel;
-      geracao ++;
-      var linha = "";
-      linha =  '<tr>';
-      pnGeracao = g1 + g2 + geracao+'a. Geração' +g3 + g4;
-      linha += '<td>' + pnGeracao + '</td>';
-      for (j = i; j < data.membros.length && data.membros[j].nivel == nivel; j++) {
-         let id = data.membros[j].id;
-         linha += '<td>' + btpai01 + 'onclick="add_pai(' + id + ',' + nivel + ')">' + data.membros[j].nome_pai + btfecha + '</td>';
-         linha += '<td>' + btmae01 + 'onclick="add_mae(' + id + ',' + nivel + ')">' + data.membros[j].nome_mae +  btfecha + '</td>';
-         linha += '<td>' + btkid01 + 'onclick="add_filho(' + id + ',' + nivel + ')">' + data.membros[j].nome + btfecha + '</td>';
-     };
-     linha += '</tr>'
-     $('#table-geracao').append(linha);              
-     i=j-1;
-    }
-  }
 
-  const add_pai = (id, nivel) => {
+  const add_pai = async (id, nivel) => {
     id_origem =  id;
     nivel_ = nivel;
-
+   
     $("#idform").show();
     $("#id_pai").show();
     $("#id_mae").hide();
@@ -136,7 +146,7 @@ const getList = async () => {
 
   }
   
-  const add_mae = (id, nivel) => {
+  const add_mae = async (id, nivel) => {
     id_origem =  id;
     nivel_ = nivel;
     $("#idform").show();
@@ -145,7 +155,7 @@ const getList = async () => {
     $("#id_filho").hide();
   }
 
-  const add_filho = (id, nivel) => {
+  const add_filho = async (id, nivel, id_pai, id_mae) => {
     id_origem =  id;
     nivel_ = nivel;
 
@@ -155,23 +165,31 @@ const getList = async () => {
     $("#id_filho").show();
   }
 
-  const cancelar = () => {
-    getListMembroComum(id_base_);
+  const cancelar = async () => {
+    await getListMembroComum(id_base_);
   }
 
-  const salvarPai = () => {
+  const salvarPai = async () => {
     nivel_atual = nivel_ - 1;
     let inputNomePai = document.getElementById("newInputPai").value;
-    postItemComum( 'http://127.0.0.1:5000/membro_comum_pai', id_origem, inputNomePai,nivel_atual);
+    await  postItemComum( 'http://127.0.0.1:5000/membro_comum_pai', id_origem, inputNomePai,nivel_atual);
     $("#idform").hide();
   }
 
-  const salvarMae = () => {
+  const salvarMae = async () => {
     nivel_atual = nivel_ - 1;
     let inputNomeMae = document.getElementById("newInputMae").value;
-    postItemComum( 'http://127.0.0.1:5000/membro_comum_mae',id_origem, inputNomeMae,nivel_atual);
+    await postItemComum( 'http://127.0.0.1:5000/membro_comum_mae',id_origem, inputNomeMae,nivel_atual);
     $("#idform").hide();
   }
+
+  const salvarFilho = async () => {
+    nivel_atual = nivel_ + 1;
+    let inputNomeMae = document.getElementById("newInputFilho").value;
+    await  postItemComum( 'http://127.0.0.1:5000/membro_comum_filho',id_origem, inputNomeMae,nivel_atual);
+    $("#idform").hide();
+  }
+
 
 
   const postItemComum = async (url, id_origem, inputNome, nivel) => {
@@ -183,7 +201,9 @@ const getList = async () => {
     formData.append('mae', 0);
     formData.append('id_origem', id_origem);
 
-    fetch(url, {
+
+
+    await  fetch(url, {
       method: 'post',
       body: formData
     })
@@ -198,8 +218,8 @@ const getList = async () => {
 
 
   
-  const deleteItem = (item) => {
-    let url = 'http://127.0.0.1:5000/produto?nome=' + item;
+  const deleteItem = async (item) => {
+    let url = await 'http://127.0.0.1:5000/produto?nome=' + item;
     fetch(url, {
       method: 'delete'
     })
